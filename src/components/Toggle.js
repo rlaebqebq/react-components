7f311c7770f6dab51d7de33633fc11109fcb063a
rlaebqebq <rlaebqebq@gmail.com>
2022-05-08 05:08:34 +0900

    refactor_Toggle

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,12 +1,12 @@
-import React from 'react';
+import styles from './Toggle.module.scss'
 
 export default function Toggle() {
   return (
     <div className='container'>
-      <input type='checkbox' name='toggle' id='toggleTitle' />
-      <label htmlFor='toggleTitle'>
-        <div className='toggleCheck' data-unchecked='기본' data-checked='상세' ></div>
+      <input type='checkbox' id='toggleTitle' />
+      <label htmlFor='toggleTitle' className={styles.toggleLabel}>
+        <div className={styles.toggleCheck} data-unchecked='기본' data-checked='상세'></div>
       </label>
     </div>
-  );
+  )
 }
