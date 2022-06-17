ab726ba378d28c2c8c2d3ee706e6023254231690
rlaebqebq <rlaebqebq@gmail.com>
2022-06-17 17:46:21 +0900

    refactor : routes 폴더 정리

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,10 +1,29 @@
-import styles from './Routes.module.scss'
-import WantedPreOnboarding from './WantedPreOnboarding'
+import Toggle from 'components/Toggle'
+import Tab from 'components/Tab'
+import Slider from 'components/Slider'
+import Input from 'components/Input'
+import Dropdown from 'components/Dropdown'
+
+import styles from './routes.module.scss'
 
 const App = () => {
   return (
     <div className={styles.appWrapper}>
-      <WantedPreOnboarding />
+      <div className={styles.componentWrapper}>
+        <Toggle />
+      </div>
+      <div className={styles.componentWrapper}>
+        <Tab />
+      </div>
+      <div className={styles.componentWrapper}>
+        <Slider />
+      </div>
+      <div className={styles.componentWrapper}>
+        <Input />
+      </div>
+      <div className={styles.componentWrapper}>
+        <Dropdown />
+      </div>
     </div>
   )
 }
